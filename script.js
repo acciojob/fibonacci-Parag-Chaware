6b036a3e52ab9a39420cfe4307acce2590a3dd97
Parag-Chaware <148242322+Parag-Chaware@users.noreply.github.com>
2024-02-14 22:23:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function fibonacci(num) {
 	let firstNum = 0;
 	let secondNum = 1;
 	let thirdNum = num;
+	if(num==1){
+		return firstNum;
+	}
+	else if(num ==2){
+		return secondNum;
+	}
+	else{
 	for(i=1; i<=num-2; i++){
 		thirdNum = firstNum + secondNum;
 		firstNum = secondNum;
 		secondNum = thirdNum;
 	}
 	return thirdNum;
+	}
 }
 
 module.exports = fibonacci;
