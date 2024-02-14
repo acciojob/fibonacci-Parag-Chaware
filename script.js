702d0c4a44bbf46f2782d0cd0372c3509fae3f03
Parag-Chaware <148242322+Parag-Chaware@users.noreply.github.com>
2024-02-14 22:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	let firstNum = 0;
+	let secondNum = 1;
+	for(i=1; i<=num; i++){
+		num = firstNum + secondNum;
+		firstNum = secondNum;
+		secondNum = num;
+	}
+	return num;
 }
 
 module.exports = fibonacci;
